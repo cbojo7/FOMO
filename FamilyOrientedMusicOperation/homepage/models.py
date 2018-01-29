e09ccdebcf496b360bb14e73b62159f9b7aa89ac
cbojo7 <chelsey.johnson7@gmail.com>
2018-01-29 16:25:50 -0700

    modified database

--- FamilyOrientedMusicOperation/homepage/models.py
+++ FamilyOrientedMusicOperation/homepage/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Account(models.Model):
+    
+    salary = models.CharField(max_length=255, blank=True)
+    birthday = models.CharField(max_length=255, blank=True)
+    address = models.CharField(max_length=255, blank=True)
+    city = models.CharField(max_length=150, blank=True)
+    state = models.CharField(max_length=2, blank=True)
+    zip = models.CharField(max_length=15, blank=True)
+
