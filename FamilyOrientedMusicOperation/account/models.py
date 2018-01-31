9382b5df8e83befd0a3ab683f95fb87fea3cd32d
cbojo7 <chelsey.johnson7@gmail.com>
2018-01-30 22:02:19 -0700

    created first test case

--- FamilyOrientedMusicOperation/account/models.py
+++ FamilyOrientedMusicOperation/account/models.py
@@ -8,6 +8,10 @@ class User(AbstractCUser):
     city = models.TextField(null=True, blank=True)
     state = models.TextField(null=True, blank=True)
     zipcode = models.TextField(null=True, blank=True)
+    first_name = models.TextField(null=True, blank=True)
+    last_name = models.TextField(null=True, blank=True)
+    username_field = models.TextField(null=True, blank=True)
+    password = models.TextField(null=True, blank=True)
 
     def get_purchases(self):
         return [ 'Roku', 'Apple TV', 'Chromecast']
--- FamilyOrientedMusicOperation/account/tests.py
+++ FamilyOrientedMusicOperation/account/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from account import models as amod
 
-# Create your tests here.
+class UserModelTest(TestCase):
+    # def setUp(self):
+    #     Animal.objects.create(name="lion", sound="roar")
+    #     Animal.objects.create(name="cat", sound="meow")
+
+    def test_user_create_save_load(self):
+        #'''tests round trip of user model data to/from database'''
+        u1 = amod.User()
+        u1.first_name = 'Marge'
+        u1.last_name = 'Simpson'
+        u1.email = 'marge@simpsons.com'
+        #u1.password = 'password
+        u1.set_password('password')
+        u1.save()
+
+        u2 = amod.User.objects.get(u1.email)
+        self.assertEquals(u1.first_name, u2.first_name)
+        self.assertEquals(u1.last_name, u2.last_name)
+        self.assertEquals(u1.email, u2.email)
+        self.assertEquals(u1.password, u2.password)
+        self.assertTrue(u2.check_password('password'))
