3ff5f56ec7a5e9a406c18f9b8ff4dc881a14d95a
cbojo7 <chelsey.johnson7@gmail.com>
2018-02-01 18:36:53 -0700

    fixed all the unit tests from class lecture

--- FamilyOrientedMusicOperation/account/tests.py
+++ FamilyOrientedMusicOperation/account/tests.py
@@ -22,28 +22,17 @@ class UserModelTest(TestCase):
         self.assertEquals(self.u1.username_field, u2.username_field)
         self.assertEquals(self.u1.password, u2.password)
         self.assertTrue(u2.check_password('password'))
-
-    def test_add_groups_check_permissions(self):
-        #'''add groups to a user and check permissions'''
-        a = 1 
-        self.assertEquals(a, 1)
        
-    def test_create_group(self):
+    def test_add_group_test_permissions(self):
         g1 = Group()
-        g1.name = 'Salespeople'
+        g1.name = 'SalesPeople'
         g1.save()
         self.u1.groups.add(g1)
         self.u1.save()
-        self.assertTrue(self.u1.group.name='SalesPeople')
-        #self.assertTrue(self.u1.groups.filter(name='SalesPeople'))
-        
-        #g1.permissions.add(self.u1.groups.get(id=g1.id).first() is not None)
-        for p in Permission.objects.all():
-            print(p.codename)
-            print(p.name)
-            print(p.content_type)
-            # self.u1.user_permissions.add(p)
-
+        #self.u1.objects.get(group.name)
+        #self.assertTrue(self.u1.group.name='SalesPeople')
+        self.assertTrue(self.u1.groups.filter(name='SalesPeople'))
+        g1.permissions.add(Permission.objects.get(id=g1.id))
         p = Permission()
         p.codename = 'change_product_price'
         p.name = 'Change the price of a product'
